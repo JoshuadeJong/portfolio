7ad29155503907567e869fcaea6851e28587a860
Joshua de Jong <joshuakdejong@gmail.com>
2020-05-20 00:39:37 -0700

    SKill icons added

--- webApp/about/routes.py
+++ webApp/about/routes.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, render_template
+from webApp.functions import get_static_json
 
 about_bp = Blueprint('about', __name__, template_folder='html')
 
 
 @about_bp.route('/about')
 def landing():
-    return render_template('landing_about.html')
+    data_about = get_static_json('about.json')
+    data_project = get_static_json('projects.json')['projects']
+    data_project.sort(key=order_projects_by_weight)
+    data_project = data_project[0:3]
+
+    return render_template('landing_about.html', projects=data_project, about=data_about)
+
+
+def order_projects_by_weight(projects):
+    try:
+        return int(projects['weight'])
+    except KeyError:
+        return 0
--- webApp/home/routes.py
+++ webApp/home/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from webApp.functions import get_static_json
 
 home_bp = Blueprint('home', __name__, template_folder='html')
 
 @home_bp.route('/')
 @home_bp.route('/home')
 def landing():
-    return render_template('landing_home.html')
+    data = get_static_json('about.json')
+    return render_template('landing_home.html', data=data)
