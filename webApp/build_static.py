ad021e85d5ddbd2eba40d4f25b172d927b99fe3e
Joshua de Jong <joshuakdejong@gmail.com>
2020-05-30 15:44:24 -0700

    Updated build_static to meet github requirements

--- webApp/build_static.py
+++ webApp/build_static.py
@@ -1,7 +1,15 @@
 from flask_frozen import Freezer
 from webApp import app
+import shutil
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-    freezer.run()
+    # Create the static pages
+    freezer.freeze()
+
+    # Move the files to the correct location for github pages
+    shutil.move('webApp/build', 'docs')
+
+
+
