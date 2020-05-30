1b6fbb8e51a6607098d500891f8483649e87da1a
Joshua de Jong <joshuakdejong@gmail.com>
2020-05-30 15:53:19 -0700

    updated deployment pipline

--- webApp/build_static.py
+++ webApp/build_static.py
@@ -8,8 +8,18 @@ if __name__ == '__main__':
     # Create the static pages
     freezer.freeze()
 
+    # Clear the docs directory
+    shutil.rmtree('docs', True)
+
     # Move the files to the correct location for github pages
     shutil.move('webApp/build', 'docs')
 
-
+    # Move css, fonts, images, js out of the static directory
+    shutil.move('docs/static/css', 'docs/css')
+    shutil.move('docs/static/fonts', 'docs/fonts')
+    shutil.move('docs/static/images', 'docs/images')
+    shutil.move('docs/static/js', 'docs/js')
+    
+    # Remove static directory
+    shutil.rmtree('docs/static', True)
 
