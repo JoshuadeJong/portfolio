4d9c965decd4b4e59d10aca2aa9c5a491d5ead5c
Joshua de Jong <joshuakdejong@gmail.com>
2020-06-02 00:32:34 -0700

    created a pages directory

--- webApp/__init__.py
+++ webApp/__init__.py
@@ -4,17 +4,17 @@ from webApp.config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from webApp.errors.handlers import errors_bp
+from webApp.pages.errors.handlers import errors_bp
 app.register_blueprint(errors_bp)
-from webApp.home.routes import home_bp  # Add Homepage
+from webApp.pages.home.routes import home_bp  # Add Homepage
 app.register_blueprint(home_bp)
-from webApp.timeline.routes import timeline_bp # Add About page
+from webApp.pages.timeline.routes import timeline_bp # Add About page
 app.register_blueprint(timeline_bp)
-from webApp.projects.routes import projects_bp # Add Projects page
+from webApp.pages.projects.routes import projects_bp # Add Projects page
 app.register_blueprint(projects_bp)
-from webApp.projects.routes import project_bp # Add Project page
+from webApp.pages.projects.routes import project_bp # Add Project page
 app.register_blueprint(project_bp)
-from webApp.about.routes import about_bp # Add About page
+from webApp.pages.about.routes import about_bp # Add About page
 app.register_blueprint(about_bp)
 
 
