6e7bfb63b5805eb2f62be3b52a4319d030beefae
Joshua de Jong <joshuakdejong@gmail.com>
2020-06-08 00:23:16 -0700

    modal scroll changed, early version of carousel created

--- webApp/static/js/modal.js
+++ webApp/static/js/modal.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
     // Here specify your content or message, enclose between <p>
-    $('.modal').modal({
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        opacity: .7, // Opacity of modal background
-        inDuration: 300, // Transition in duration
-        outDuration: 200, // Transition out duration
-        startingTop: '4%', // Starting top style attribute
-        endingTop: '10%', // Ending top style attribute
-        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-        alert("Ready");
-        console.log(modal, trigger);
-        },
-        complete: function() { alert('Closed'); } // Callback for Modal close
+    $('.modal').modal(
+        {
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .7, // Opacity of modal background
+            inDuration: 300, // Transition in duration
+            outDuration: 200, // Transition out duration
+            startingTop: '0%', // Starting top style attribute
+            endingTop: '0%', // Ending top style attribute
+            preventScrolling: true, // Prevent page from scrolling while modal is open.
+            ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                alert("Ready");
+                console.log(modal, trigger);
+            },
+            complete: function() { // Callback for Modal close
+                alert('Closed');
+            }
         }
     );
+
 });
+
