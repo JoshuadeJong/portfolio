dc34001e9d9a2e859cc407bfd57b1c3bee277a00
Joshua de Jong <joshuakdejong@gmail.com>
2020-06-18 01:46:29 -0700

    Added ability to choose highlighted project using the about.json

--- webApp/pages/about/routes.py
+++ webApp/pages/about/routes.py
@@ -1,15 +1,26 @@
 from flask import Blueprint, render_template
 from webApp.functions import get_static_json
+import json
 
 about_bp = Blueprint('about', __name__, template_folder='html')
 
 
 @about_bp.route('/about.html')
 def landing():
+    # General about page info
     data_about = get_static_json('about.json')
-    data_project = get_static_json('projects.json')['projects']
-    data_project.sort(key=order_projects_by_weight)
-    data_project = data_project[0:3]
+
+    # Query for Projects
+    if "projects" in data_about:
+        data_project = get_static_json('projects.json')['projects']
+        project_list = data_about['projects']
+
+        for i in range(len(data_project) - 1, -1, -1):
+            if data_project[i]['name'] not in project_list:
+                data_project.pop(i)
+
+    else:
+        data_project = None
 
     return render_template('landing_about.html', projects=data_project, about=data_about)
 
